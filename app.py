ed9b25b6aa120fabd7f5b665603a49c3d7bb05f0
smyu24 <67177134+smyu24@users.noreply.github.com>
2021-04-17 14:11:54 -0700

    Updated Version 1

--- app.py
+++ app.py
@@ -1,27 +1,58 @@
-from flask import Flask, render_template, request
 from wtforms import Form, FloatField, validators
-from compute import compute
-#import model
+#from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, redirect, render_template, request, abort, url_for, session
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
 
 class InputForm(Form):
     r = FloatField()
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+# Ensure templates are auto-reloaded -------------------------------------------
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-@app.route("/form1", methods=["GET", "POST"])
+app.secret_key = "Seung-min_Yu_Sean_Kim_MMT_Math_Worksheet_Generator_Website"
+#------------------------------------------------------------------------
+#db = SQLAlchemy(app)
+
+#class Comment(db.Model):
+#
+#    __tablename__ = "comments"
+#
+#    id = db.Column(db.Integer, primary_key=True)
+#    content = db.Column(db.String(4096))
+
+@app.route('/')
 def index():
-    form = InputForm(request.form)
-    if request.method == "POST":
-        r = form.r.data
-        s = compute(r)
-        print("aaa")
-        return render_template("viewoutput.html", form = form, s = s)
-    else:
-        return render_template("viewoutput.html", form = form)
+    """The "main" page of the website"""
+    return render_template("index.html", error=False)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Login page of the website"""
+    return render_template("login.html", error=True)
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+    # Forget any user_id
+    return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register the user"""
+    return render_template("register.html", error=False)
+
+
+@app.route('/pre-algebra')
+def prealgebra():
+    """pre-algebra"""
+    return render_template("pre-algebra.html", error=False)
+
 
 if __name__ == "__main__":
   app.run()
--- compute.py
+++ compute.py
@@ -1,3 +1,34 @@
 import math
 def compute(r):
     return(math.sin(r))
+
+
+#####THIS PART IS THE APP.PY
+
+from flask import Flask, render_template, request
+from wtforms import Form, FloatField, validators
+from compute import compute
+#import model
+
+class InputForm(Form):
+    r = FloatField()
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/form1", methods=["GET", "POST"])
+def index():
+    form = InputForm(request.form)
+    if request.method == "POST":
+        r = form.r.data
+        s = compute(r)
+        print("aaa")
+        return render_template("viewoutput.html", form = form, s = s)
+    else:
+        return render_template("viewoutput.html", form = form)
+
+if __name__ == "__main__":
+  app.run()
