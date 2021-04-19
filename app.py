c2ae560e6eda7c065a1f47430217afda47d43030
seankmmt <seank@mmtprep.com>
2021-04-19 00:45:17 -0700

    v1

--- app.py
+++ app.py
@@ -1,54 +1,21 @@
+#requisites
 from flask import Flask, Blueprint, render_template, abort, Markup
 from jinja2 import TemplateNotFound
 
+#logic package imports
+
 app = Flask(__name__)
 
-# set to True to inform that the app needs to be re-created
-to_reload = False
+# Using a production configuration
+app.config.from_object('config.ProdConfig')
+
+# Using a development configuration
+#app.config.from_object('config.DevConfig')
+
+@app.route("/")
+def index():
+    return  render_template('index.html')
 
 
-def get_app():
-    print("create app now")
-    app = Flask(__name__)
-
-    # to make sure of the new app instance
-    now = datetime.now()
-
-    @app.route("/")
-    def index():
-        return f"hello, the app started at %s" % now
-
-    @app.route('/reload')
-    def reload():
-        global to_reload
-        to_reload = True
-        return "reloaded"
-
-    return app
-
-
-class AppReloader(object):
-    def __init__(self, create_app):
-        self.create_app = create_app
-        self.app = create_app()
-
-    def get_application(self):
-        global to_reload
-        if to_reload:
-            self.app = self.create_app()
-            to_reload = False
-
-        return self.app
-
-    def __call__(self, environ, start_response):
-        app = self.get_application()
-        return app(environ, start_response)
-
-
-# This application object can be used in any WSGI server
-# for example in gunicorn, you can run "gunicorn app"
-application = AppReloader(get_app)
-
-if __name__ == '__main__':
-    run_simple('localhost', 5000, application,
-               use_reloader=True, use_debugger=True, use_evalex=True)
+if __name__=='__main__':
+    app.run(debug=False)
